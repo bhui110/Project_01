0e88f0b029ac085d0bea8dbaddc7c6d1abdd85a7
brandon hui <brandonlikesstuff@gmail.com>
2025-11-12 13:29:32 -0800

    added create drop methods to main

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from models import Base
 from database import Session
 from models import Product
 
+import sys
+
 def create():
     pass
 
@@ -11,11 +13,20 @@ def drop():
 
 
 def main():
-    Base.metadata.create_all(engine)
-    product = Product(name="eggs", price=12.34, available=100)
-    session = Session()
-    session.add(product)
-    session.commit()
+    try:
+        if sys.arv[1] == "create":
+            create()
+        elif sys.arv[1] == "drop":
+            drop()
+        else:
+            raise ValueError("not valid argument..")
+        Base.metadata.create_all(engine)
+        product = Product(name="eggs", price=12.34, available=100)
+        session = Session()
+        session.add(product)
+        session.commit()
+    except Exception as e:
+        print(e)
 
 
 
